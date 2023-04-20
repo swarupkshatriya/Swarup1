7fbc408a820f191d02f5c98e4db3c1f2d2ab3024
swarupkshatriya <swarup.kshatriya@gmail.com>
2023-04-20 19:36:48 +0530

    Created the Class3 and added the Sysout in class two

---
+++ Swarup1/src/TestingSteps/TestingClass3.java
@@ -0,0 +1,5 @@
+package TestingSteps;
+
+public class TestingClass3 {
+
+}
--- Swarup1/src/TestingSteps/TestingClassTwo.java
+++ Swarup1/src/TestingSteps/TestingClassTwo.java
@@ -5,5 +5,6 @@ public class TestingClassTwo {
 		public static void display(){
 			System.out.println("Hello");
 			System.out.println("I am swarup");
+			System.out.println("I am adding the new sysout statement");
 		}
 }
